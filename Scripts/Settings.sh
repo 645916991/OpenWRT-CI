a284158ced0d26bb3d1d31b893f464cabdd56067
645916991 <77471759+645916991@users.noreply.github.com>
2024-09-25 08:11:18 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -46,54 +46,6 @@ if [[ $WRT_TARGET == *"IPQ"* ]]; then
 	echo "CONFIG_ATH11K_MEM_PROFILE_512M=y" >> ./.config
 fi
 # 添加luci-app-dockerman、docker-compose
-#!/bin/bash
-
-#修改默认主题
-sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
-#修改immortalwrt.lan关联IP
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-#添加编译日期标识
-sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_CI-$WRT_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
-#修改默认WIFI名
-sed -i "s/\.ssid=.*/\.ssid=$WRT_WIFI/g" $(find ./package/kernel/mac80211/ ./package/network/config/ -type f -name "mac80211.*")
-
-CFG_FILE="./package/base-files/files/bin/config_generate"
-#修改默认IP地址
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
-#修改默认主机名
-sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
-
-# 设置wifi加密方式为psk2+ccmp,wifi密码为88888889
-# sed -i 's/encryption=none/encryption=psk2+ccmp/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-# sed -i '/set wireless.default_radio${devidx}.encryption=psk2+ccmp/a\\t\t\tset wireless.default_radio${devidx}.key=QQ858952311' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-# 设置无线的国家代码为CN,wifi的默认功率为20
-# sed -i 's/country=US/country=CN/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-# sed -i '/set wireless.radio${devidx}.disabled=0/a\\t\t\tset wireless.radio${devidx}.txpower=20' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-# 设置wan口上网方式为PPPOE，本地编译时在文件的第86行左右
-#  sed -i 's/2:-dhcp/2:-pppoe/g' package/base-files/files/lib/functions/uci-defaults.sh
-# 设置PPPOE上网的账号和密码,本地编译时在文件的第182和183行左右
-#  sed -i 's/username='"'"'username'"'"'/username='"'"'601'"'"'/g; s/password='"'"'password'"'"'/password='"'"'18639812928'"'"'/g' package/base-files/files/bin/config_generate
-#配置文件修改
-echo "CONFIG_PACKAGE_luci=y" >> ./.config
-echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
-echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
-echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
-
-#手动调整的插件
-if [ -n "$WRT_PACKAGE" ]; then
-	echo "$WRT_PACKAGE" >> ./.config
-fi
-
-#高通平台锁定512M内存
-if [[ $WRT_TARGET == *"IPQ"* ]]; then
-	echo "CONFIG_IPQ_MEM_PROFILE_1024=n" >> ./.config
-	echo "CONFIG_IPQ_MEM_PROFILE_512=y" >> ./.config
-	echo "CONFIG_ATH11K_MEM_PROFILE_1G=n" >> ./.config
-	echo "CONFIG_ATH11K_MEM_PROFILE_512M=y" >> ./.config
-fi
-# 添加luci-app-dockerman、docker-compose
 echo "CONFIG_PACKAGE_luci-app-dockerman=y" >> ./.config
 echo "CONFIG_PACKAGE_docker-compose=y" >> ./.config
 # 添加openssh-sftp-server使得finalshell得以看见文件列表
