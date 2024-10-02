029c4bf32f8de07419ec85f048f1687fe7e76c37
645916991 <77471759+645916991@users.noreply.github.com>
2024-10-02 22:05:15 +0800

    Update feeds.sh

--- Scripts/feeds.sh
+++ Scripts/feeds.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # 追加kiddin9源
-echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git" >> feeds.conf.default
+# echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git" >> feeds.conf.default
